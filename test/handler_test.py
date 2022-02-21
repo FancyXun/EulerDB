f1f23de9d9a378c1aba068a1d6295d317a7ba22c
fancyxun <837633751@qq.com>
2022-02-21 17:21:14 +0800

    update test handler table

--- test/handler_test.py
+++ test/handler_test.py
@@ -17,38 +17,37 @@ content = {
 }
 
 
-def request_api():
-    return
+table = 'test'
 
 
 sql_list = {
     'where':
-        ['select * from test limit 5',
-         'select id_card, name, age from test where age = 20 limit 5',
-         'select id_card, name, age, score  from test where score = 60 limit 5',
-         'select id_card, name, age from test where age > 40 limit 100',
-         'select id_card, name, age from test where age < 10 limit 100',
-         'select id_card, name, age from test where age <= 10 limit 100',
+        ['select * from {} limit 5'.format(table),
+         'select id_card, name, age from {} where age = 20 limit 5'.format(table),
+         'select id_card, name, age, score  from {} where score = 60 limit 5'.format(table),
+         'select id_card, name, age from {} where age > 40 limit 100'.format(table),
+         'select id_card, name, age from {} where age < 10 limit 100'.format(table),
+         'select id_card, name, age from {} where age <= 10 limit 100'.format(table),
          ],
     'order_by_min_max':
-        ['select * from test where id_card = "496715970993917044442778" and name = "iezlcpnjws"',
-         'select id_card, name, age from test order by age limit 5',
-         'select distinct id_card, name, age from test limit 5',
-         'select distinct age, name, id_card from test limit 1000',
-         'select max(age), min(age) from test',
-         'select id_card, name from test WHERE age = 30 limit 100',
-         'select max(score), min(score) from test '
+        ['select * from {} where id_card = "496715970993917044442778" and name = "iezlcpnjws"'.format(table),
+         'select id_card, name, age from {} order by age limit 5'.format(table),
+         'select distinct id_card, name, age from {} limit 5'.format(table),
+         'select distinct age, name, id_card from {} limit 1000'.format(table),
+         'select max(age), min(age) from {}'.format(table),
+         'select id_card, name from {} WHERE age = 30 limit 100'.format(table),
+         'select max(score), min(score) from {} '.format(table)
          ],
     'like':
-        ['select id_card, name from test where name like "rax%fpb%" limit 5'],
+        ['select id_card, name from {} where name like "rax%fpb%" limit 5'.format(table)],
     'count':
-        ['select count(*) from test'],
+        ['select count(*) from {}'.format(table)],
     'delete':
-        ['DELETE FROM test WHERE id_card = "496715970993917044442778" and name = "iezlcpnjws" '],
+        ['DELETE FROM {} WHERE id_card = "496715970993917044442778" and name = "iezlcpnjws" '.format(table)],
     'alter':
-        ['ALTER TABLE test ADD Birthday date'],
+        ['ALTER TABLE {} ADD Birthday date'.format(table)],
     'update':
-        ['UPDATE test set id_card = "496715970993917044442778" , name = "iezlcpnjws" WHERE age = 30']
+        ['UPDATE {} set id_card = "496715970993917044442778" , name = "iezlcpnjws" WHERE age = 30'.format(table)]
 
 }
 
@@ -56,9 +55,9 @@ sql_list = {
 class TestPostHandler(TestCase):
 
     def test_handler_create_table(self):
-        create_table_sql = 'create table if not exists test(' \
+        create_table_sql = 'create table if not exists {}(' \
                            'id_card varchar(40), name varchar(20), age int, sex varchar(5), ' \
-                           'score int, nick_name varchar(20), comments varchar(200));'
+                           'score int, nick_name varchar(20), comments varchar(200));'.format(table)
 
         encrypted_columns = {
             "id_card": {
@@ -83,13 +82,13 @@ class TestPostHandler(TestCase):
 
     def test_handler_insert_table(self):
         for i in range(10000):
-            query = 'insert into test(id_card, name, age, sex, score, nick_name, comments) values ( "' + str(
+            query = 'insert into {}(id_card, name, age, sex, score, nick_name, comments) values ( "' + str(
                 random.randint(1000000000000000000, 1000000000000000000000000)) + '","' + ''.join(
                 random.sample('zyxwvutsrqponmlkjihgfedcba', 10)) + '",' + str(random.randint(1, 50)) + ', "' + ''.join(
                 random.sample('fm', 1)) + '",' + str(random.randint(60, 100)) + ',"' + ''.join(
                 random.sample('zyxwvutsrqponmlkjihgfedcba', 10)) + '","' + ''.join(
                 random.sample('zyxwvutsrqpondgsghgsfgyftuywiecvdbhsbdhgshfdgsgfysgmlkjihgfedcba', 30)) + '")'
-            content['query'] = query
+            content['query'] = query.format(table)
             json_data = json.dumps(content)
             requests.post('http://localhost:8888/query', json_data)
 
