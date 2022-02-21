c43497652050ab000615ad3273ff7a24ab4e0c87
fancyxun <837633751@qq.com>
2022-02-21 17:09:45 +0800

    update test handler

--- test/handler_test.py
+++ test/handler_test.py
@@ -10,11 +10,53 @@ user = 'root'
 password = 'root'
 port = 3306
 
+content = {
+    'host': db_host, 'db': db,
+    'user': user, 'password': password,
+    'port': port
+}
+
+
+def request_api():
+    return
+
+
+sql_list = {
+    'where':
+        ['select * from test limit 5',
+         'select id_card, name, age from test where age = 20 limit 5',
+         'select id_card, name, age, score  from test where score = 60 limit 5',
+         'select id_card, name, age from test where age > 40 limit 100',
+         'select id_card, name, age from test where age < 10 limit 100',
+         'select id_card, name, age from test where age <= 10 limit 100',
+         ],
+    'order_by_min_max':
+        ['select * from test where id_card = "496715970993917044442778" and name = "iezlcpnjws"',
+         'select id_card, name, age from test order by age limit 5',
+         'select distinct id_card, name, age from test limit 5',
+         'select distinct age, name, id_card from test limit 1000',
+         'select max(age), min(age) from test',
+         'select id_card, name from test WHERE age = 30 limit 100',
+         'select max(score), min(score) from test '
+         ],
+    'like':
+        ['select id_card, name from test where name like "rax%fpb%" limit 5'],
+    'count':
+        ['select count(*) from test'],
+    'delete':
+        ['DELETE FROM test WHERE id_card = "496715970993917044442778" and name = "iezlcpnjws" '],
+    'alter':
+        ['ALTER TABLE test ADD Birthday date'],
+    'update':
+        ['UPDATE test set id_card = "496715970993917044442778" , name = "iezlcpnjws" WHERE age = 30']
+
+}
+
 
 class TestPostHandler(TestCase):
 
     def test_handler_create_table(self):
-        create_table_sql = 'create table if not exists user(' \
+        create_table_sql = 'create table if not exists test(' \
                            'id_card varchar(40), name varchar(20), age int, sex varchar(5), ' \
                            'score int, nick_name varchar(20), comments varchar(200));'
 
@@ -32,255 +74,48 @@ class TestPostHandler(TestCase):
                 "arithmetic": False
             }
         }
-        content = {
-            'host': db_host, 'db': db,
-            'user': user, 'password': password,
-            'port': port,
-            'query': create_table_sql,
-            'encrypted_columns': encrypted_columns}
-
-        json_data = json.dumps(content)
-        resp = requests.post('http://localhost:8888/query', json_data)
-        print(resp.json()['result'])
-
-    def test_handler_query_limit(self):
-        content = {
-            'host': db_host, 'db': db,
-            'user': user, 'password': password,
-            'port': port,
-            'query': 'select * from user limit 5'}
+        content['query'] = create_table_sql
+        content['encrypted_columns'] = encrypted_columns
 
         json_data = json.dumps(content)
         resp = requests.post('http://localhost:8888/query', json_data)
         print(resp.json()['result'])
 
     def test_handler_insert_table(self):
-        for i in range(10):
-            query = 'insert into user(id_card, name, age, sex, score, nick_name, comments) values ( "' + str(
+        for i in range(10000):
+            query = 'insert into test(id_card, name, age, sex, score, nick_name, comments) values ( "' + str(
                 random.randint(1000000000000000000, 1000000000000000000000000)) + '","' + ''.join(
                 random.sample('zyxwvutsrqponmlkjihgfedcba', 10)) + '",' + str(random.randint(1, 50)) + ', "' + ''.join(
                 random.sample('fm', 1)) + '",' + str(random.randint(60, 100)) + ',"' + ''.join(
                 random.sample('zyxwvutsrqponmlkjihgfedcba', 10)) + '","' + ''.join(
                 random.sample('zyxwvutsrqpondgsghgsfgyftuywiecvdbhsbdhgshfdgsgfysgmlkjihgfedcba', 30)) + '")'
-
-            content = {
-                'host': db_host, 'db': db,
-                'user': user, 'password': password,
-                'port': port,
-                'query': query}
-
+            content['query'] = query
             json_data = json.dumps(content)
             requests.post('http://localhost:8888/query', json_data)
 
-    def test_insert_one_row(self):
-        # fixme
-        query = "insert user(id_card, name, age, sex, score, nick_name, comments) " \
-                "values (" \
-                "'496715970993917044442778', 'iezlcpnjws', 20, 'm', 92, " \
-                "'duriolzvwj', 'sswgyyydewbxsgzsduggstfhnrgsvj')"
-        content = {
-            'host': db_host, 'db': db,
-            'user': user, 'password': password, 'port': port,
-            'query': query}
-
-        json_data = json.dumps(content)
-        requests.post('http://localhost:8888/query', json_data)
-
-    def test_handler_query_where(self):
-        content = {
-            'host': db_host, 'db': db,
-            'user': user, 'password': password, 'port': port,
-            'query': 'select id_card, name, age from user where age = 20 limit 5'}
-
-        json_data = json.dumps(content)
-        resp = requests.post('http://localhost:8888/query', json_data)
-        print(resp.json()['result'])
-
-    def test_handler_query_greater(self):
-        content = {
-            'host': db_host, 'db': db,
-            'user': user, 'password': password, 'port': port,
-            'query': 'select id_card, name, age from user where age > 40 limit 100'}
-
-        json_data = json.dumps(content)
-        resp = requests.post('http://localhost:8888/query', json_data)
-        print(resp.json()['result'])
-
-    def test_handler_query_greater_equal(self):
-        content = {
-            'host': db_host, 'db': db,
-            'user': user, 'password': password, 'port': port,
-            'query': 'select id_card, name, age from user where age >= 40 limit 100'}
-
-        json_data = json.dumps(content)
-        resp = requests.post('http://localhost:8888/query', json_data)
-        print(resp.json()['result'])
-
-    def test_handler_query_less(self):
-        content = {
-            'host': db_host, 'db': db,
-            'user': user, 'password': password, 'port': port,
-            'query': 'select id_card, name, age from user where age < 10 limit 100'}
-
-        json_data = json.dumps(content)
-        resp = requests.post('http://localhost:8888/query', json_data)
-        print(resp.json()['result'])
-
-    def test_handler_query_less_equal(self):
-        content = {
-            'host': db_host, 'db': db,
-            'user': user, 'password': password, 'port': port,
-            'query': 'select id_card, name, age from user where age <= 10 limit 100'}
-
-        json_data = json.dumps(content)
-        resp = requests.post('http://localhost:8888/query', json_data)
-        print(resp.json()['result'])
-
-    def test_handler_query_where_and(self):
-        content = {
-            'host': db_host, 'db': db,
-            'user': user, 'password': password, 'port': port,
-            'query': 'select * from user where id_card = "496715970993917044442778" and name = "iezlcpnjws"'}
-
-        json_data = json.dumps(content)
-        resp = requests.post('http://localhost:8888/query', json_data)
-        print(resp.json()['result'])
-
-    def test_handler_query_order_by(self):
-        content = {
-            'host': db_host, 'db': db,
-            'user': user, 'password': password, 'port': port,
-            'query': 'select id_card, name, age from user order by age limit 5'}
-
-        json_data = json.dumps(content)
-        resp = requests.post('http://localhost:8888/query', json_data)
-        print(resp.json()['result'])
-
-    def test_handler_query_distinct(self):
-        content = {
-            'host': db_host, 'db': db,
-            'user': user, 'password': password, 'port': port,
-            'query': 'select distinct id_card, name, age from user limit 5'}
-
-        json_data = json.dumps(content)
-        resp = requests.post('http://localhost:8888/query', json_data)
-        print(resp.json()['result'])
-
-    def test_handler_query_distinct1(self):
-        content = {
-            'host': db_host, 'db': db,
-            'user': user, 'password': password, 'port': port,
-            'query': 'select distinct age, name, id_card from user limit 1000'}
-
-        json_data = json.dumps(content)
-        resp = requests.post('http://localhost:8888/query', json_data)
-        print(resp.json()['result'])
-
-    def test_handler_query_max(self):
-        content = {
-            'host': db_host, 'db': db,
-            'user': user, 'password': password, 'port': port,
-            'query': 'select max(age) from user'}
-
-        json_data = json.dumps(content)
-        resp = requests.post('http://localhost:8888/query', json_data)
-        print(resp.json()['result'])
-
-    def test_handler_query_min(self):
-        content = {
-            'host': db_host, 'db': db,
-            'user': user, 'password': password, 'port': port,
-            'query': 'select min(age) from user'}
-
-        json_data = json.dumps(content)
-        resp = requests.post('http://localhost:8888/query', json_data)
-        print(resp.json()['result'])
-
-    def test_handler_query_like(self):
-        content = {
-            'host': db_host, 'db': db,
-            'user': user, 'password': password, 'port': port,
-            'query': 'select id_card, name from user where name like "rax%fpb%" limit 5'}
-
-        json_data = json.dumps(content)
-        resp = requests.post('http://localhost:8888/query', json_data)
-        print(resp.json()['result'])
-
-    def test_handler_query_max1(self):
-        content = {
-            'host': db_host, 'db': db,
-            'user': user, 'password': password, 'port': port,
-            'query': 'select max(score) from user '}
-
-        json_data = json.dumps(content)
-        resp = requests.post('http://localhost:8888/query', json_data)
-        print(resp.json()['result'])
-
-    def test_handler_query_min1(self):
-        content = {
-            'host': db_host, 'db': db,
-            'user': user, 'password': password, 'port': port,
-            'query': 'select min(score) from user'}
-
-        json_data = json.dumps(content)
-        resp = requests.post('http://localhost:8888/query', json_data)
-        print(resp.json()['result'])
-
-    def test_handler_alter_table(self):
-        content = {
-            'host': db_host, 'db': db,
-            'user': user, 'password': password, 'port': port,
-            'query': 'ALTER TABLE user ADD Birthday date'}
-        json_data = json.dumps(content)
-        requests.post('http://localhost:8888/query', json_data)
-
-    def test_handler_count(self):
-        content = {
-            'host': db_host, 'db': db,
-            'user': user, 'password': password, 'port': port,
-            'query': 'select count(*) from user'}
-        json_data = json.dumps(content)
-        resp = requests.post('http://localhost:8888/query', json_data)
-        print(resp.json()['result'])
-
-    def test_handler_delete_row(self):
-        content = {
-            'host': db_host, 'db': db,
-            'user': user, 'password': password, 'port': port,
-            'query': 'DELETE FROM user WHERE id_card = "496715970993917044442778" and name = "iezlcpnjws" '}
-        json_data = json.dumps(content)
-        resp = requests.post('http://localhost:8888/query', json_data)
-        print(resp.json()['result'])
-
-    def test_handler_update(self):
-        content = {
-            'host': db_host, 'db': db,
-            'user': user, 'password': password, 'port': port,
-            'query': 'UPDATE user set id_card = "496715970993917044442778" , name = "iezlcpnjws" '
-                     'WHERE age = 30 '}
-        json_data = json.dumps(content)
-        resp = requests.post('http://localhost:8888/query', json_data)
-        print(resp.json()['result'])
-
-    def test_handler_after_update_select(self):
-        content = {
-            'host': db_host, 'db': db,
-            'user': user, 'password': password, 'port': port,
-            'query': 'select id_card, name from user '
-                     'WHERE age = 30 limit 100'}
-        json_data = json.dumps(content)
-        resp = requests.post('http://localhost:8888/query', json_data)
-        print(resp.json()['result'])
+    def test_handler_sql(self):
+        for key, value in sql_list.items():
+            print("---{}...".format(key))
+            for v in value:
+                content['query'] = v
+                json_data = json.dumps(content)
+                try:
+                    resp = requests.post('http://localhost:8888/query', json_data)
+                    if 'result' in resp.json():
+                        print(resp.json()['result'])
+                except Exception as e:
+                    print(e)
+                    continue
 
 
 class TestRewriterHandler(TestCase):
 
     def test_select_rewrite(self):
-        content = {
-            'query': 'select id_card, name, age from user where age = 20 limit 5',
+        sql = {
+            'query': 'select id_card, name, age from test where age = 20 limit 5',
             'db': db
         }
-        json_data = json.dumps(content)
+        json_data = json.dumps(sql)
         resp = requests.post('http://localhost:8888/rewrite_query', json_data)
         print(resp.text)
 
